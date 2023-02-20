97e7c1bda5407b961f28fbe99802a0571fdf28b4
Ken Moini <ken@kenmoini.com>
2023-02-20 03:13:03 -0500

    ugh

---
+++ docs/linode-k8s.md
@@ -0,0 +1,4 @@
+# Deploying LKE (Linode Kubernetes Engine)
+
+1. Create a Cluster :upside_down_face: 
+2. Create a Firewall that defaults to drop on the nodes except for SSH and inter-subnet talk
---
+++ init-cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create namespace argocd
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl apply -n argocd -f argocd/bootstrap
